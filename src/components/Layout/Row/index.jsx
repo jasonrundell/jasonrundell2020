be956261c5f2e435db1584b993e889fd652d4fc0
Jason Rundell <jason.rundell@gmail.com>
2020-03-01 22:02:54 -0500

    Row justify props added

--- src/components/Layout/Row/index.jsx
+++ src/components/Layout/Row/index.jsx
@@ -1,13 +1,51 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { classNames } from '../../../utils/css'
+
 import styles from './Row.module.scss'
 
-const Row = ({ children }) => {
-  return <div className={styles.root}>{children}</div>
+const Row = ({ justify, children }) => {
+  let justifyClassName = 'flex-start'
+
+  switch (justify) {
+    case 'start':
+      justifyClassName = 'start'
+      break
+    case 'end':
+      justifyClassName = 'end'
+      break
+    case 'space-around':
+      justifyClassName = 'spaceAround'
+      break
+    case 'space-between':
+      justifyClassName = 'spaceBetween'
+      break
+    case 'space-evenly':
+      justifyClassName = 'spaceEvenly'
+      break
+    default:
+      justifyClassName = justify
+  }
+
+  const className = classNames(styles.root, styles[justifyClassName])
+
+  return <div className={className}>{children}</div>
+}
+
+Row.defaultProps = {
+  justify: 'start',
 }
 
 Row.propTypes = {
+  justify: PropTypes.oneOf([
+    'start',
+    'end',
+    'center',
+    'space-around',
+    'space-between',
+    'space-evenly',
+  ]),
   children: PropTypes.any.isRequired,
 }
 
