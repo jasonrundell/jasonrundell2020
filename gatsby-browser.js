3588920a7bc043a64c6e27eb707f08ad3bb2499b
Jason Rundell <jason.rundell@gmail.com>
2020-03-05 00:34:36 -0500

    normalize.css changed for jrun-normalize

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 // Normalize for best cross-browser and device support
-import './node_modules/normalize.css/normalize.css'
+import './node_modules/jrun-normalize/normalize.css'
 // Import my global CSS
 import './src/styles/main.scss'
