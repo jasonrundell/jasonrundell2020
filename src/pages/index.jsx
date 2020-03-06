6c59df9a8e6ff00149f37ac3a805db7ed5070a89
Jason Rundell <jason.rundell@gmail.com>
2020-03-06 13:09:11 -0500

    Copy updates

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -31,26 +31,31 @@ export default ({ data }) => {
           </Row>
           <Row>
             <Paragraph>
-              Yo! I'm a developer who loves learning and using the latest in
-              front end web development.
+              Hello. I'm a developer who loves learning and using the latest in
+              front end web development. My skill set includes experience with
+              React, Gatsby, AEM, Contentful, WordPress, Git, Gulp, Grunt,
+              Parcel, PHP, MySQL, SASS, CSS, HTML, JavaScript, and Amazon Web
+              Services.
             </Paragraph>
             <Paragraph>
               My love of building web experiences started in my high school’s
               library in 1997 with GeoCities and it’s been rewarding ever since!
-              As you can imagine, with over 20 years of love for HTML, I have
+              As you can imagine, with over 20 years of love for the web, I have
               been a part of a wide variety of web projects: from iframes, to
-              Flash, jQuery Mobile, and now ‘isomorphic JavaScript
-              applications’. Whatever the latest trend is, I’m either deeply
-              involved or experimenting with it in my spare time and imagining
-              what’s next. My skills and experiences are deep, wide-ranging, and
-              I am always seeking new best practices and methodologies. I
-              embrace change, crave challenge, and love technology!
+              Flash, WordPress multisites, jQuery Mobile, custom CMS
+              applications, a Facebook contest platform, React design systems,
+              and now JAMstack with Gatsby and headless CMSs. Whatever the
+              latest trend is, I’m either deeply involved or experimenting with
+              it in my spare time. My skills and experiences are deep,
+              wide-ranging, and I am always seeking new best practices and
+              methodologies. I embrace change, crave challenge, and love
+              technology!
             </Paragraph>
           </Row>
         </Section>
         <Section>
           <Row>
-            <Title>About</Title>
+            <Title>Seasoned and Capable</Title>
           </Row>
           <Row>
             <Paragraph>
