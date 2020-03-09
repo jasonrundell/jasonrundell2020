db4c211628e125a967c26bf8a9cefa58e29378c7
Jason Rundell <jason.rundell@gmail.com>
2020-03-09 06:49:49 -0400

    UnorderedList and OrderedList now have isHorizontal prop

--- src/components/Lists/OrderedList/index.jsx
+++ src/components/Lists/OrderedList/index.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { classNames } from '../../../utils/css'
+
 import styles from './OrderedList.module.scss'
 
-const OrderedList = ({ items, children }) => {
+const OrderedList = ({ items, children, isHorizontal }) => {
+  const className = classNames(styles.root, isHorizontal && styles.horizontal)
   return (
-    <ol className={styles.root}>
+    <ol className={className}>
       {items.map((item, index) => (
         <li className={styles.listItem} key={index}>
-          {item}{children}
+          {item}
+          {children}
         </li>
       ))}
     </ol>
   )
 }
 
+OrderedList.defaultProps = {
+  isHorizontal: false,
+}
+
 OrderedList.propTypes = {
   items: PropTypes.array.isRequired,
-  children: PropTypes.any
+  children: PropTypes.any,
+  isHorizontal: PropTypes.bool,
 }
 
 export default OrderedList
--- src/components/Lists/UnorderedList/index.jsx
+++ src/components/Lists/UnorderedList/index.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { classNames } from '../../../utils/css'
+
 import styles from './UnorderedList.module.scss'
 
-const UnorderedList = ({ items, children }) => {
+const UnorderedList = ({ items, children, isHorizontal }) => {
+  const className = classNames(styles.root, isHorizontal && styles.horizontal)
   return (
-    <ul className={styles.root}>
+    <ul className={className}>
       {items.map((item, index) => (
         <li className={styles.listItem} key={index}>
-          {item}{children}
+          {item}
+          {children}
         </li>
       ))}
     </ul>
   )
 }
 
+UnorderedList.defaultProps = {
+  isHorizontal: false,
+}
+
 UnorderedList.propTypes = {
   items: PropTypes.array.isRequired,
-  children: PropTypes.any
+  children: PropTypes.any,
+  isHorizontal: PropTypes.bool,
 }
 
 export default UnorderedList
