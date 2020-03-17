46328f153336b84bab283421d7171b3277181802
Jason Rundell <jason.rundell@gmail.com>
2020-03-17 16:38:39 -0400

    Reverting

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,12 +75,7 @@ module.exports = {
     },
     {
       resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: process.env.CONTENTFUL_HOST,
-        environment: process.env.CONTENTFUL_ENVIRONMENT_ID,
-      },
+      options: contentfulConfig,
     },
   ],
 }
