28510de5efb5d332c413b263ece39a0c071d2ab6
Jason Rundell <jason.rundell@gmail.com>
2020-04-01 11:40:27 -0400

    Function simplified

--- src/utils/graphql.js
+++ src/utils/graphql.js
@@ -1,10 +1,3 @@
 export function edgesToObject(edgesData) {
-  const nodes = edgesData.map(item => {
-    return item.node
-  })
-  const finalObject = nodes.map(item => {
-    return item
-  })
-
-  return finalObject
+  return edgesData.map(({ node }) => node)
 }
