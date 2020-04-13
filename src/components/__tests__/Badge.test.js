bd646998b3e0fb3e25a763b3d844ce150efcdb6a
Jason Rundell <jason.rundell@gmail.com>
2020-04-13 16:11:53 -0400

    Badge test updated

--- src/components/__tests__/Badge.test.js
+++ src/components/__tests__/Badge.test.js
@@ -1,12 +1,51 @@
 import React from 'react'
-import { create } from 'react-test-renderer'
+import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from 'react-dom/test-utils'
 import { catchWarnings } from '../../utils/test-utils'
 
 import Badge from '../Badge'
 
+catchWarnings()
+
+let container = null
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null
+})
+
 describe('Badge component', () => {
-  test('Matches the snapshot', () => {
-    const component = create(<Badge icon="Icon" />)
-    expect(component.toJSON()).toMatchSnapshot()
+  it('Badge required icon prop', () => {
+    act(() => {
+      render(<Badge icon="icon" />, container)
+    })
+    expect(container.textContent).toBe('icon')
+  })
+
+  it('Badge isInverse prop', () => {
+    act(() => {
+      render(
+        <Badge icon="icon" isInverse={true}>
+          icon
+        </Badge>,
+        container
+      )
+    })
+    expect(container.textContent).toBe('icon')
+
+    act(() => {
+      render(
+        <Badge icon="icon" isInverse={false}>
+          icon
+        </Badge>,
+        container
+      )
+    })
+    expect(container.textContent).toBe('icon')
   })
 })
