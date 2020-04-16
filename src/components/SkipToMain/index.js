9957c6c75578a25ee1f7e52d5db63fdf2f58ed25
Jason Rundell <jason.rundell@gmail.com>
2020-04-16 18:26:26 -0400

    Renamed class

--- src/components/SkipToMain/index.js
+++ src/components/SkipToMain/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import styles from './SkipToMain.module.scss'
 
 export default () => (
-  <a href="#main" className={styles.link}>
+  <a href="#main" className={styles.root}>
     Skip to main content
   </a>
 )
