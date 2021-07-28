7fedde7e073a041a21a418477ee950808bbfe3db
Jason Rundell <jason.rundell@gmail.com>
2021-07-28 01:04:59 -0400

    Update to how title component works

--- src/components/Typography/Title/index.jsx
+++ src/components/Typography/Title/index.jsx
@@ -4,8 +4,30 @@ import PropTypes from 'prop-types'
 import { h1, h2, h3, h4 } from './Title.module.scss'
 
 const Title = ({ level, children }) => {
-  const Component = `h${level}`
-  return <Component className={[Component]}>{children}</Component>
+  let titleClass
+  let Component
+  switch (level) {
+    case 1:
+      titleClass = h1
+      Component = 'h1'
+      break
+    case 2:
+      titleClass = h2
+      Component = 'h2'
+      break
+    case 3:
+      titleClass = h3
+      Component = 'h3'
+      break
+    case 4:
+      titleClass = h4
+      Component = 'h4'
+      break
+    default:
+      titleClass = h1
+      Component = 'h1'
+  }
+  return <Component className={titleClass}>{children}</Component>
 }
 
 Title.defaultProps = {
